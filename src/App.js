fe88cd10e762b7cb82ea984864742c45f1f947e9
Luoxi Pan <l.panpax@gmail.com>
2023-02-09 13:08:14 +1100

    Add basic form input for entering tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import React from 'react';
 import Overview from './components/Overview';
 
-function App() {
-  return (
-    <Overview />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+      tasks: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({inputText: e.target.value});
+  }
+
+  handleSubmit(e) {
+    alert('Tasks Array: ' + this.state.inputText);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <Overview />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Task Description:
+            <input type="text" name="taskInput" value={this.state.text} onChange={this.handleChange} />
+            <input type="submit" name="formSubmit" value="Submit" />
+          </label>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,9 +1,16 @@
-function Overview(props) {
-    return (
-        <div>
-            <p>Hello World</p>
-        </div>
-    );
+import React from 'react';
+
+/* Overview renders the tasks */
+class Overview extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h3>Tasks</h3>
+        );
+    }
 }
 
 export default Overview;
